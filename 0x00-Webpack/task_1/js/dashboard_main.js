1cbb678a51dcd55b0580ee5123f891cc54956cdf
Anphimens <philipinamensah19@gmail.com>
2024-08-11 18:10:10 +0100

    Contains jquery import

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,29 @@
+import $ from "jquery";
+import _ from "lodash";
+
+// Initialize the click counter
+let clickCount = 0;
+
+// function to update the counter and display the count
+function updateCounter() {
+  clickCount++;
+  $("#count").text(`${clickCount} clicks on the button`);
+}
+
+// Debounce the updateCounter function to limit how often it's called
+const debouncedUpdateCounter = _.debounce(updateCounter, 300);
+
+// Add paragraph elements
+$(document).ready(function () {
+  $("body").append("<p>Holberton Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+  // Add button element
+  $("body").append("<button>Click here to get started</button>");
+  // Add paragraph element with id 'count'
+  $("body").append('<p id="count"></p>');
+  // Add copyright paragraph element
+  $("body").append("<p>Copyright - Holberton School</p>");
+
+  // bind the debouncedUpdateCounter function to the button click event
+  $("start-btn").on("click", debouncedUpdateCounter);
+});
