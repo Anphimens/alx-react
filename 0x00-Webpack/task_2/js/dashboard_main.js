cf9e569572a2e9111faeafbf2e418136ccd22970
Anphimens <philipinamensah19@gmail.com>
2024-08-14 21:01:49 +0100

    css imported

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,5 +1,6 @@
 import $ from "jquery";
 import _ from "lodash";
+import "../css/main.css";
 
 // Initialize the click counter
 let clickCount = 0;
@@ -11,14 +12,15 @@ function updateCounter() {
 }
 
 // Debounce the updateCounter function to limit how often it's called
-const debouncedUpdateCounter = _.debounce(updateCounter, 300);
+const debouncedUpdateCounter = _.debounce(updateCounter, 500);
 
 // Add paragraph elements
 $(document).ready(function () {
+  $("body").append('<div id="logo"></div>');
   $("body").append("<p>Holberton Dashboard</p>");
   $("body").append("<p>Dashboard data for the students</p>");
   // Add button element with an ID
-  $("body").append("<button>Click here to get started</button>");
+  $("body").append('<button id="start-btn">Click here to get started</button>');
   // Add paragraph element with id 'count'
   $("body").append('<p id="count"></p>');
   // Add copyright paragraph element
