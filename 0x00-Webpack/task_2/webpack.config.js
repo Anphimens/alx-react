75f27d6a44d0ba8990a8d9461051a9a340d426de
Anphimens <philipinamensah19@gmail.com>
2024-08-14 15:02:49 +0100

    config updated

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: path.resolve(__dirname, "/js/dashboard_main.js"),
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
